cf021f0d0932a2beb10f3ab0f6f28fa59205a483
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-03-23 14:59:26 -0400

    Initial Commit

--- Lab15.cpp
+++ Lab15.cpp
@@ -7,30 +7,29 @@ double computeWays(int, int);
 double factorial(int);
 
 /*******************************************************************
-* getLotteryInfo *
+* getLotteryInfo                                                   *
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
+********************************************************************/
+
+
+
+/*******************************************************************
+* computeWays                                                      *
+* Computes and returns the number of different possible sets       *
+* of k numbers that can be chosen from a set of n numbers.         *
+* The formula for this is     k!(n- k)!                            *
+*                             --------                             *
+*                                 n!                               *
+* Note that the computation is done in a way that does not require *
+* multiplying two factorials together. This is done to prevent any *
+* intermediate result becoming so large that it causes overflow.   *
+********************************************************************/
+
+
+/*******************************************************************
+* factorial                                                        *
+* This function computes factorials recursively.                   *
 *******************************************************************/
-void getInfo( int & pickFrom, int & numPicks){
 
-}
 
-/******************************************************************
-* computeWays                                                     *
-* Computes and returns the number of different possible sets      *
-* of k numbers that can be chosen from a set of n numbers.        *
-* The formula for this is     k!(n- k)!                           *
-*                             --------                            *
-*                                 n!                              *
-*******************************************************************/
-// Note that the computation is done in a way that does not require
-// multiplying two factorials together. This is done to prevent any
-// intermediate result becoming so large that it causes overflow.
-
-double computeWays( int n, int k){
-    
-}
-
-double factorial(int n){
-
-}
