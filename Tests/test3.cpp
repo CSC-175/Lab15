515f79ef5e05b5f082e1c616e9e26d8098428846
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-03-23 15:02:26 -0400

    Delete Tests/test3.cpp

--- Tests/test3.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-# include "../Lab15.cpp"
-using namespace std;
-
-int main()
-{
-    cout << factorial(0) << " " << factorial(5) << " " << factorial(8) << endl;
-    return 0;
-}
